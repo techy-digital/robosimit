7fd1c1ebbe260f81a6d2415216e93ad1d6cf076d
A. Taha Arslan <taha@taha.tech>
2022-02-09 22:41:30 +0300

    Early GUI created.

---
+++ gui.py
@@ -0,0 +1,72 @@
+# This Python file uses the following encoding: utf-8
+import sys, logging
+from PyQt5 import QtWidgets, QtGui
+from PyQt5 import uic
+import subprocess, re
+
+logfile = 'gui.log'
+open(logfile, 'w').close()
+logging.basicConfig(filename=logfile, 
+                    filemode='a', 
+                    format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
+
+class QTextEditLogger(logging.Handler):
+    def __init__(self, parent):
+        super().__init__()
+        self.widget = QtWidgets.QPlainTextEdit(parent)
+        self.widget.setReadOnly(True)
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.widget.appendPlainText(msg)
+
+class RoboSimITWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("robosimit.ui", self)
+        self.logTextBox = QTextEditLogger(self)
+        # You can format what is printed to text box
+        self.logTextBox.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        logging.getLogger().addHandler(self.logTextBox)
+        # You can control the logging level
+        logging.getLogger().setLevel(logging.DEBUG)
+        self.logTextBox.widget.setGeometry(10, 550, 1000, 200)
+        self.dockerready = False
+        self.imageready = False
+        self.pushButton.clicked.connect(self.clickme)
+        self.prechecks()
+    
+    def clickme(self):
+        logging.debug("clicked.")
+
+    def prechecks(self):
+        command = ['/usr/bin/docker', 'info']
+        logging.debug(command)
+        res = subprocess.check_output(command)
+        string = res.decode("utf-8")
+        logging.debug(string)
+        regex = re.compile('Server Version.*\n')
+        m = regex.search(string)
+        if m is not None:
+            self.dockerready = True
+            self.DockerReadyResultLabel.setText('Ready ☑️' + '\n' + m.group(0))
+            logging.debug(m.group(0))
+        
+        command = ['docker', 'images', 'valu3s:robosimit']
+        res = subprocess.check_output(command)
+        string = res.decode("utf-8")
+        print(string)
+        regex = re.compile('valu3s')
+        m = regex.search(string)
+        if m is not None:
+            self.imageready = True
+            self.ContainerImageReadyResultLabel.setText('Ready ☑️')
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = RoboSimITWindow()
+    window.show()
+    sys.exit(app.exec_())
